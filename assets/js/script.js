a07092f55ba0511eb511f4e51565a05d0423f2d8
elfsvet <stepanmatysik@gmail.com>
2022-04-17 00:24:13 -0400

    functional is ready we need error handling and styling with bootstrap or bulma

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //weather personal APIKey ending 8f22ad49c151f0bf1918e3b3dbe2b739
 var apiKey = "8f22ad49c151f0bf1918e3b3dbe2b739";
-
-// jquery references
+// ! jquery references--
 var userFormEl = $("#user-form");
 var buttonUserFormEl = $("#user-form .btn");
 var cityNameInputEl = $("#city-name");
@@ -9,23 +8,22 @@ var searchedCitiesEl = $(".searched-cities");
 var displayCityNameEl = $(".display-city-name");
 var cityButtonEl = $(".searched-city-button");
 var oneDayForecastEl = $('.one-day-forecast')
-// variables
+var currentWeatherEl = $("#current-weather");
+var fiveDayEl = $("#five-day");
+// ! variables--
 var latitude;
 var longitude;
 var cityNameFromInput;
-
 var cities = [];
-
+// ! --functions--
 var loadCities = function () {
     // get an array with cities key from localStorage
     var citiesLoad = localStorage.getItem("cities");
     // if we don't have any cities keys in localStorage
     if (!citiesLoad) {
-        console.log("nothing to load, exit loadCities")
         // finish the call
         return false;
     } else {
-        console.log("parsing the localstorage")
         citiesLoad = JSON.parse(citiesLoad);
         for (var i = 0; i < citiesLoad.length; i++) {
             // need to display the buttons inside searched cities
@@ -47,12 +45,7 @@ var displaySearchedCities = function (city) {
     var cityButton = "<button class='searched-city-button button is-light'>" + city + "</button>"
     // add city for the search history here
     displayCityNameEl.html(city);
-    // create this button using jquery
-    // searchedCitiesEl.add("button", city).addClass("searched-city-button button is-light");
-
     searchedCitiesEl.append(cityButton);
-
-
 }
 
 
@@ -79,22 +72,18 @@ var formSubmitHandler = function (event) {
 
             // getCoordinates(city);
 
-
+            getCoordinates(city);
             displaySearchedCities(city);
 
             cityNameInputEl.val("");
 
         } else {
-            // and don't add it to button
             alert("Please enter a new city");
-            // add remove values
+            // add remove values from input line
             cityNameInputEl.val("");
         }
         //add if city doesn't exist error handler
         // needs to display the same as clicked with its name as a city parameter
-
-        console.log("in form submit " + cities)
-
     } else {
         alert("Please enter a city you would like to look up weather for");
     }
@@ -110,12 +99,12 @@ var getCoordinates = function (cityName) {
         // console.log(latitude);
         longitude = data[0].lon;
         // getCityWeatherRepo(latitude, longitude);
-        getFiveDayForecast(latitude, longitude, cityName);
+        getForecast(latitude, longitude, cityName);
     })
 };
 
 // gets data for 8 days need to iterate just for 5 0<5
-var getFiveDayForecast = function (latitude, longitude, cityName) {
+var getForecast = function (latitude, longitude, cityName) {
     var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&exclude=minutely,hourly&appid=" + apiKey;
     $.get(weatherApi, function (data) {
         console.log("Data of the latitude longitude " + latitude + " and " + longitude)
@@ -139,80 +128,96 @@ var capitalizeString = function (str) {
 
 
 var displayCurrentWeather = function (data, cityName) {
-
+    // ! variables --
     var day = 0;
-    displayCityNameEl.html(cityName);
     var dayToDisplay = data.daily[day]
     var date = dateConvert(dayToDisplay.dt);
     var condition = capitalizeString(dayToDisplay.weather[0].description);
     var iconLink = 'http://openweathermap.org/img/wn/' + dayToDisplay.weather[0].icon + '.png'
     var temp = Math.round(dayToDisplay.temp.day);
-    var windSpeed = dayToDisplay.wind_speed;
+    var wind = dayToDisplay.wind_speed;
     var humidity = dayToDisplay.humidity;
-    var uvIndex = dayToDisplay.uvi;
+    var uv = dayToDisplay.uvi;
+    // ! references --
 
-    var currentWthrEl = $("#current-weather");
-
-    displayCityNameEl.html(cityName);
-
-
-    currentWthrEl.children(".date").html(date);
-
-    // icon of the weather
-    currentWthrEl.children(".condition").attr('src', iconLink).attr('alt', condition);
-
-    var oneDFEl = currentWthrEl.children(".one-day-forecast");
-
-
-    oneDFEl.children('.temp').html('Temp: ' + temp + ' F');
-    oneDFEl.children(".wind").html('Wind: ' + windSpeed + ' MPH')
-
-    oneDFEl.children(".humidity").html('Humidity: ' + humidity + ' %')
-    // uv index
-    oneDFEl.children(".uv-index").html('UV Index: ' + uvIndex)
-
-
+    if (!currentWeatherEl.children(".city").length) {
+        currentWeatherEl.append('<h3 class="city">' + cityName + '</h3>');
+        currentWeatherEl.append('<span class="date">(' + date + ')</span>');
+        currentWeatherEl.append('<img class="icon" src="' + iconLink + '" alt="' + condition + '"></ >');
+        currentWeatherEl.append('<div class="one-day"></div>');
+        var oneDay = currentWeatherEl.children(".one-day");
+        oneDay.append('<p class="temp">Temp: ' + temp + ' F</p>');
+        oneDay.append('<p class="wind">Wind: ' + wind + ' MPH</p>');
+        oneDay.append('<p class="humidity">Humidity: ' + humidity + ' %</p>');
+        oneDay.append('<p class="uv">UV Index: ' + uv + '</p>');      
+    } else {
+        currentWeatherEl.children(".city").html(cityName);
+        currentWeatherEl.children(".date").html("("+date+")");
+        currentWeatherEl.children(".icon").attr("src", iconLink).attr("alt", condition);
+        currentWeatherEl.children(".one-day").children(".temp").html('Temp: ' + temp + ' F');
+        currentWeatherEl.children(".one-day").children(".wind").html('Wind: ' + wind + ' MPH');
+        currentWeatherEl.children(".one-day").children(".humidity").html('Humidity: ' + humidity + ' %');
+        currentWeatherEl.children(".one-day").children(".uv").html('UV Index: ' + uv);
+    }
 }
-
 var displayFiveDayForecast = function (data, cityName) {
     for (var day = 1; day < 6; day++) {
-        //sets city name at the top
-        displayCityNameEl.html(cityName);
         // create a variable 
-        var dayToDisplay = data.daily[day]
+        var dayToDisplay = data.daily[day];
         var date = dateConvert(dayToDisplay.dt);
         var condition = capitalizeString(dayToDisplay.weather[0].description);
-        var iconLink = 'http://openweathermap.org/img/wn/' + dayToDisplay.weather[0].icon + '.png'
+        var iconLink = 'http://openweathermap.org/img/wn/' + dayToDisplay.weather[0].icon + '.png';
         var temp = Math.round(dayToDisplay.temp.day);
-        var windSpeed = dayToDisplay.wind_speed;
+        var wind = dayToDisplay.wind_speed;
         var humidity = dayToDisplay.humidity;
-        var uvIndex = dayToDisplay.uvi;
+        var uv = dayToDisplay.uvi;
+        var id = "#card-" + day;
+        // if first time run
+        if (!fiveDayEl.children("#card-5").length) {
+            if (!fiveDayEl.children(".title").length) {
+                fiveDayEl.append('<h3 class="title">5-Day Forecast</h3>');
+            }
+            fiveDayEl.append('<div id="card-' + day + '" class="card"></div>');
+            $(id).append('<p class="date">' + date + '</p>');
+            $(id).append('<img class="icon" src="' + iconLink + '" alt="' + condition + '"></ >');
+            $(id).append('<div class="one-day"></div>');
+            $(id).append('<p class="temp">Temp: ' + temp + ' F</p>');
+            $(id).append('<p class="wind">Wind: ' + wind + ' MPH</p>');
+            $(id).append('<p class="humidity">Humidity: ' + humidity + ' %</p>');
+            $(id).append('<p class="uv">UV Index: ' + uv + '</p>');
+        } else {
+            $(id).children('.date').html(date);
+            $(id).children('.icon').html(date);
+            $(id).children('.temp').html('Temp: ' + temp + ' F');
+            $(id).children('.wind').html('Wind: ' + wind + ' MPH');
+            $(id).children('.humidity').html('Humidity: ' + humidity + ' %');
+            $(id).children('.uv').html('UV Index: ' + uv);
+        }
 
+        // var fiveDFEl = $(".five-day-forecast")
+        // // need display 
 
-        var fiveDFEl = $(".five-day-forecast")
-        // need display 
-
-        //date
-        fiveDFEl.children(".date").html(date);
-        // text of the condition
-        // var conditionDiscription = capitalizeString(dayToDisplay.weather[0].description);
-        // icon of the weather
-        fiveDFEl.children(".condition").attr('src', iconLink).attr('alt', condition);
-        // temperature day temp
-        fiveDFEl.children(".temp").html('Temp: ' + temp + ' F')
-        // wind speed
-        fiveDFEl.children(".wind").html('Wind: ' + windSpeed + ' MPH')
-        // humidity
-        fiveDFEl.children(".humidity").html('Humidity: ' + humidity + ' %')
-        // uv index
-        fiveDFEl.children(".uv-index").html('UV Index: ' + uvIndex)
+        // //date
+        // fiveDFEl.children(".date").html(date);
+        // // text of the condition
+        // // var conditionDiscription = capitalizeString(dayToDisplay.weather[0].description);
+        // // icon of the weather
+        // fiveDFEl.children(".condition").attr('src', iconLink).attr('alt', condition);
+        // // temperature day temp
+        // fiveDFEl.children(".temp").html('Temp: ' + temp + ' F')
+        // // wind speed
+        // fiveDFEl.children(".wind").html('Wind: ' + wind + ' MPH')
+        // // humidity
+        // fiveDFEl.children(".humidity").html('Humidity: ' + humidity + ' %')
+        // // uv index
+        // fiveDFEl.children(".uv-index").html('UV Index: ' + uv)
     }
 }
 
 
-// uploadd cities stored in local storage
+// upload cities stored in local storage
 loadCities();
-// add a click event for old search saved cities to direct to the same formsubmitHandler with their information.
+// add a click event for old search saved cities to direct to the same formSubmitHandler with their information.
 // remove line with city if none was called yet
 displayCityNameEl.html("");
 userFormEl.on("submit", formSubmitHandler);
